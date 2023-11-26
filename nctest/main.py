928e3fedcfcf1ca01cfd272407b0eca5f8deea7b
전강현 <aniren@debian.moe>
2023-11-27 00:22:34 +0900

    Add files via upload
    RedstarOSU's mediaserver read_audio() 함수에 적용한 DT, NC, HF 기능 실험 코드

---
+++ nctest/main.py
@@ -0,0 +1,36 @@
+import os
+from mutagen.mp3 import MP3
+
+file_list = os.listdir(f"./")
+file_list_audio = [file for file in file_list if file.endswith(".mp3")]
+
+def songinfo(i):
+    audio = MP3(i)
+    print(audio.info.sample_rate)
+    return audio.info.sample_rate
+
+def DT():
+    for i in file_list_audio:
+        ffmpeg_msg = f'ffmpeg -i "songs\{i}" -af atempo=1.5 -y "DT\{i[:-4]}-DT.mp3"'
+        print(f"ffmpeg_msg = {ffmpeg_msg}")
+        os.system(ffmpeg_msg)
+
+def NC():
+    for i in file_list_audio:
+        pitch = songinfo(i)
+        ffmpeg_msg = f'ffmpeg -i "songs\{i}" -af asetrate={pitch}*1.5 -y "NC\{i[:-4]}-NC.mp3"'
+        print(f"ffmpeg_msg = {ffmpeg_msg}")
+        os.system(ffmpeg_msg)
+
+def HF():
+    for i in file_list_audio:
+        ffmpeg_msg = f'ffmpeg -i "songs\{i}" -af atempo=0.75 -y "HF\{i[:-4]}-HF.mp3"'
+        print(f"ffmpeg_msg = {ffmpeg_msg}")
+        os.system(ffmpeg_msg)
+
+DT()
+NC()
+HF()
+
+""" for i in file_list_audio:
+    songinfo(i) """
