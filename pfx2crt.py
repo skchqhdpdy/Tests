261dca4cc61d19be0a49c66332aba660829ae920
전강현 <aniren@debian.moe>
2024-11-13 07:42:50 +0900

    Update pfx2crt.py

--- pfx2crt.py
+++ pfx2crt.py
@@ -9,7 +9,9 @@ files = {'certFile': (file_list, open(file_list, 'rb'))}
 body = {'certTypeFrom': 'pfx', 'certTypeTo': 'pem'}
 
 s = requests.post(url, files=files, data=body, headers={"User-Agent": "pfx2crt.py"})
-s = s.content if s.status_code == 200 else print(f"{s.status_code} | 종료함!"); exit()
+print(f"{file_list} --> {s.status_code} | www.sslshopper.com 요청 완료!")
+if s.status_code == 200: s = s.content
+else: print(f"{s.status_code} | 종료함!"); exit()
 cert = s[s.find(b"-----BEGIN CERTIFICATE-----"):]
 key = s[s.find(b"-----BEGIN PRIVATE KEY-----"):s.find(b"-----END PRIVATE KEY-----")+len(b"-----END PRIVATE KEY-----")]
 with open("cert.crt", "wb") as f: f.write(cert.rstrip(b"\n"))
