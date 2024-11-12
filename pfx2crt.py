ac0fde6034a1e582057d25eda1cc4d4749640669
전강현 <aniren@debian.moe>
2024-11-13 08:29:58 +0900

    rdpSSL 적용

--- pfx2crt.py
+++ pfx2crt.py
@@ -17,3 +17,11 @@ key = s[s.find(b"-----BEGIN PRIVATE KEY-----"):s.find(b"-----END PRIVATE KEY----
 with open("cert.crt", "wb") as f: f.write(cert.rstrip(b"\n"))
 with open("cert.key", "wb") as f: f.write(key)
 print(f"{round(time.time() - st, 2)} Sec")
+
+changeRDPSSL = input("Replace RDP SSL? (y/n, (n)) : ").lower().startswith("y")
+if changeRDPSSL:
+    os.system(f'certutil -f -importpfx "{file_list}"'); time.sleep(1)
+    sha1 = os.popen('powershell -Command "Get-ChildItem -Path Cert:\\LocalMachine\\My | Where-Object { $_.Subject -match \\"redstar.moe\\" } | Select-Object -ExpandProperty Thumbprint"').read().strip()
+    print(sha1)
+    #os.system(f'reg add "HKLM\Software\Microsoft\Terminal Server Client" /v "ServerAuthentication" /t REG_SZ /d "{sha1}" /f')
+    os.system(f'reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v "SSLCertificateSHA1Hash" /t REG_BINARY /d "{sha1}" /f')
